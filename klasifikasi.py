ef0a64069b1ec9aee830d41498a73aa68e6f5452
AuliaaZhs <bilashalsa21@gmail.com>
2023-12-29 14:52:42 +0700

    Final

--- klasifikasi.py
+++ klasifikasi.py
@@ -17,13 +17,13 @@ def klasifikasi():
             age = st.number_input('Input nilai Age', min_value=0, max_value=110,step=1)
             impluse = st.number_input('Input nilai Impluse', min_value=0, max_value=1200,step=1)
             pressurelow = st.number_input('Input nilai Pressure Low', min_value=0, max_value=500,step=1)
-            kcm = st.number_input('Input nilai KCM')
+            kcm = st.number_input('Input nilai KCM', format="%.3f")
             
         with col2:
             gender = st.selectbox('Select Gender', ['Male', 'Female'])
             pressurehight = st.number_input('Input nilai Pressure High', min_value=0, max_value=500,step=1)
             glucose = st.number_input('Input nilai Glucose', min_value=0, max_value=1000,step=1)
-            troponin = st.number_input('Input nilai Troponin')
+            troponin = st.number_input('Input nilai Troponin', format="%.3f")
 
         data = {'age': age, 'gender': 1 if gender == 'Male' else 0, 'impluse': impluse, 'pressurehight': pressurehight,
                      'pressurelow': pressurelow, 'glucose': glucose, 'kcm': kcm, 'troponin': troponin}
@@ -57,9 +57,95 @@ def klasifikasi():
     if st.button('Predict'):
         user_result = rf.predict(user_data_scaled)
         st.subheader('Your Report : ')
-        output = ''
+
         if user_result[0] == 0:
-            output = 'You are healthy'
+            st.write('#### **Anda Sehat!**\n')
+            
+            input_abnormal = False
+            if input_abnormal or (
+                user_data['pressurehight'].iloc[0] > 120
+                or user_data['pressurelow'].iloc[0] > 80
+                or user_data['glucose'].iloc[0] < 70
+                or user_data['glucose'].iloc[0] > 100
+                or user_data['kcm'].iloc[0] > 25
+                or user_data['troponin'].iloc[0] > 0.04
+            ):
+                st.write('\nNamun terdapat faktor yang bisa menyebabkan Anda terkena penyakit jantung.')
+                st.write('Oleh karena itu, terdapat beberapa saran untuk Anda:\n')
+
+                if user_data['pressurehight'].iloc[0] > 120:
+                    st.write("##### **Tekanan Darah Tinggi (Sistolik) anda di atas 120mmHg.**")
+                    st.write("Berikut ini beberapa saran untuk anda:")
+                    st.write("- Anda dapat mencoba untuk mengontrol stres")
+                    st.write("- Menghindari faktor risiko tambahan seperti merokok dan mengonsumsi alkohol\n")
+            
+                if user_data['pressurelow'].iloc[0] > 80:
+                    st.write("##### **Tekanan Darah Rendah (Diastolik) anda di atas 80mmHg.**")
+                    st.write("Berikut ini beberapa saran untuk anda:")
+                    st.write("- Pertimbangkan untuk mengurangi konsumsi garam")
+                    st.write("- Periksa dan kendalikan kadar kolesterol Anda")
+                    st.write("- Tingkatkan aktivitas fisik Anda\n")
+
+                if user_data['glucose'].iloc[0] < 70:
+                    st.write("##### **Nilai Glukosa Saat Puasa Anda kurang dari 70 mg/dL.**")
+                    st.write("Berikut ini beberapa saran untuk anda:")
+                    st.write("- Anda dapat mengatur pola makan")
+                    st.write("- Pertimbangkan untuk meningkatkan asupan karbohidrat sehat\n")
+
+                if user_data['glucose'].iloc[0] > 100:
+                    st.write("##### **Nilai Glukosa Saat Puasa Anda lebih dari 100 mg/dL.**")
+                    st.write("Berikut ini beberapa saran untuk anda:")
+                    st.write("- Pertimbangkan untuk mmengurangi konsumsi gula")
+                    st.write("- Anda dapat meningkatkan aktivitas fisik\n")
+
+                if user_data['kcm'].iloc[0] > 25:
+                    st.write("##### **Nilai Creatine Kinase-MB Anda lebih dari 25 U/L.**")
+                    st.write("Berikut ini beberapa saran untuk anda:")
+                    st.write("- Pertimbangkan untuk melakukan pemeriksaan lanjutan terkait fungsi jantung")
+                    st.write("- Jaga pola hidup sehat dan hindari faktor risiko yang dapat memengaruhi kesehatan jantung\n")
+
+                if user_data['troponin'].iloc[0] > 0.04:
+                    st.write("##### **Nilai Troponin Anda lebih dari 0.04 ng/mL.**")
+                    st.write("Berikut ini beberapa saran untuk anda:")
+                    st.write("- Konsultasi dengan dokter untuk evaluasi lebih lanjut")
+                    st.write("- Jaga pola hidup sehat dan hindari faktor risiko yang dapat memengaruhi kesehatan jantung\n")
+
         else:
-            output = 'You are not healthy'
-        st.write(output)
+            st.write('#### **Anda Tidak Sehat!**\n')
+
+            if user_data['pressurehight'].iloc[0] > 120:
+                st.write("##### **Tekanan Darah Tinggi (Sistolik) anda di atas 120mmHg.**")
+                st.write("Berikut ini beberapa saran untuk anda:")
+                st.write("- Anda dapat mencoba untuk mengontrol stres")
+                st.write("- Menghindari faktor risiko tambahan seperti merokok dan mengonsumsi alkohol\n")
+            
+            if user_data['pressurelow'].iloc[0] > 80:
+                st.write("##### **Tekanan Darah Rendah (Diastolik) anda di atas 80mmHg.**")
+                st.write("Berikut ini beberapa saran untuk anda:")
+                st.write("- Pertimbangkan untuk mengurangi konsumsi garam")
+                st.write("- Periksa dan kendalikan kadar kolesterol Anda")
+                st.write("- Tingkatkan aktivitas fisik Anda\n")
+
+            if user_data['glucose'].iloc[0] < 70:
+                st.write("##### **Nilai Glukosa Saat Puasa Anda kurang dari 70 mg/dL.**")
+                st.write("Berikut ini beberapa saran untuk anda:")
+                st.write("- Anda dapat mengatur pola makan")
+                st.write("- Pertimbangkan untuk meningkatkan asupan karbohidrat sehat\n")
+
+            if user_data['glucose'].iloc[0] > 100:
+                st.write("##### **Nilai Glukosa Saat Puasa Anda lebih dari 100 mg/dL.**")
+                st.write("Berikut ini beberapa saran untuk anda:")
+                st.write("- Pertimbangkan untuk mmengurangi konsumsi gula")
+                st.write("- Anda dapat meningkatkan aktivitas fisik\n")
+
+            if user_data['kcm'].iloc[0] > 25:
+                st.write("##### **Nilai Creatine Kinase-MB Anda lebih dari 25 U/L.**")
+                st.write("Berikut ini beberapa saran untuk anda:")
+                st.write("- Pertimbangkan untuk melakukan pemeriksaan lanjutan terkait fungsi jantung")
+                st.write("- Jaga pola hidup sehat dan hindari faktor risiko yang dapat memengaruhi kesehatan jantung\n")
+
+            if user_data['troponin'].iloc[0] > 0.04:
+                st.write("##### **Nilai Troponin Anda lebih dari 0.04 ng/mL.**")
+                st.write("Berikut ini beberapa saran untuk anda:")
+                st.write("- Konsultasi dengan dokter untuk evaluasi lebih lanjut")
+                st.write("- Jaga pola hidup sehat dan hindari faktor risiko yang dapat memengaruhi kesehatan jantung\n")
